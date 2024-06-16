0076dbda393bedfc8c2b1ce5d0fd0711be5c4d0e
varunSurisetty <varunpoly143@gmail.com>
2024-06-16 19:32:24 +0530

    three files for fibanocci series

--- app.java
+++ app.java
@@ -1,5 +1,16 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        int a=0;
+        int b=1;
+        System.out.println(a); 
+        int sum=0;
+        while (sum<100) 
+        {
+        sum=a+b;
+        a=b;
+        b=sum;
+        System.out.println(sum); 
+        }
+
     }
 }
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 a=0
 b=1
 sum=0
-print(a,b)
+print(a)
 while(sum<100):
     sum=a+b
     a=b
