02b2a87e07685d827903f5f5e51a9ebe36fa589e
varunSurisetty <varunpoly143@gmail.com>
2024-06-17 12:36:23 +0530

    final push

--- fibanocci/app.js
+++ fibanocci/app.js
@@ -3,10 +3,10 @@ console.log('Hello World');
 
 // take input from the user
 const number = 100
-let n1 = 0, n2 = 1, nextTerm;
+let n1 = 0, n2 = 1, nextTerm=0;
 
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; nextTerm <= 100; i++) {
     console.log(n1);
     nextTerm = n1 + n2;
     n1 = n2;
