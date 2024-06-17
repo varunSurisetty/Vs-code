88cb5d6cb8c564e5da9444ec2a60b4b3e89b0583
varunSurisetty <varunpoly143@gmail.com>
2024-06-17 20:20:52 +0530

    Linked List 1st program

---
+++ LinkedLists/constructor.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+my_linked_list_item1 = LinkedList(4)
+
+print(my_linked_list_item1.head.value)
+print(my_linked_list_item1.tail.value)
+#print(my_linked_list_item2.head.value)
+#print(my_linked_list_item2.tail.value)
